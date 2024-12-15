8f5b6a2cff43901d2644387dc4e82533f07c352e
abdullozoda_oo8 <abdullozodaanush8@gmail.com>
2024-12-15 17:26:58 +0500

    create

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void chek(int a)
+        {
+            var d = a;
+            if (d % 2 == 0)
+            {
+                Console.WriteLine("chetnoe");
+            }
+            else
+            {
+                Console.WriteLine("ne chetnoe");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            chek(1);
+            chek(2);
+        }
+    }
+}
